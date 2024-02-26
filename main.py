560c7bfc21760f07043e76da5c2a07522ca5e0c2
FrogsNCode <frog3r23@gmail.com>
2024-02-26 18:58:53 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#we ask the user to enter two numbers
+number1 = float(input("Enter the first number:"))
+number2 = float(input("Enter the second number:"))
+
+#Let's calculate the sum of the two numbers
+sum = number1 + number2
+
+# We print the sum
+print("The sum of", number1, "and", number2, "is:", sum)
